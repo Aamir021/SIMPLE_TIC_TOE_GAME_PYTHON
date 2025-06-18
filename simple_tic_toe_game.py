7bbb138b9cf3e905a91ac27cfdc6366e35fdd4af
Aamir <aamirh54321@gmail.com>
2025-06-18 18:06:10 +0530

    simple_tic_toe_game.py

--- simple_tic_toe_game.py
+++ simple_tic_toe_game.py
@@ -10,9 +10,9 @@ def print_board():
 
 def check_winner(player):
     win_conditions = [
-        [0,1,2], [3,4,5], [6,7,8],  # Rows
-        [0,3,6], [1,4,7], [2,5,8],  # Columns
-        [0,4,8], [2,4,6]            # Diagonals
+        [0,1,2], [3,4,5], [6,7,8],  # For Rows
+        [0,3,6], [1,4,7], [2,5,8],  # For Columns
+        [0,4,8], [2,4,6]            # For Diagonals
     ]
     for condition in win_conditions:
         if all(board[i] == player for i in condition):
